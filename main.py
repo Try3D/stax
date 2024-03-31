0c69f2566bf34f8a070a5b2940a839268f2787f5
try3d <rsarans186@gmail.com>
2024-03-31 09:24:15 +0530

    Reorder

--- main.py
+++ main.py
@@ -1,5 +1,58 @@
 from sys import argv
-from stax.command_handler import execute_commands
+
+
+class Stack:
+    def __init__(self) -> None:
+        self.stack = []
+
+    def push(self, n: int) -> None:
+        self.stack += [n]
+
+    def pop(self) -> int:
+        return self.stack.pop()
+
+    def print(self) -> None:
+        print(self.stack[-1])
+
+    def add(self) -> int:
+        a = self.pop()
+        b = self.pop()
+        c = a + b
+
+        self.push(c)
+        return c
+
+    def subtract(self) -> int:
+        a = self.pop()
+        b = self.pop()
+        c = a - b
+
+        self.push(c)
+        return c
+
+    def multiply(self) -> int:
+        a = self.pop()
+        b = self.pop()
+        c = a * b
+
+        self.push(c)
+        return c
+
+    def divide(self) -> int:
+        a = self.pop()
+        b = self.pop()
+        c = a // b
+
+        self.push(c)
+        return c
+
+    def mod(self) -> int:
+        a = self.pop()
+        b = self.pop()
+        c = a % b
+
+        self.push(c)
+        return c
 
 
 def main():
@@ -25,6 +78,53 @@ def main():
 
     for line in lines:
         execute_commands(line, stacks)
+
+
+def execute_commands(line, stacks):
+    line = line.strip()
+
+    if not line:
+        return
+
+    tokens = line.split()
+
+    if len(tokens) == 1 and tokens[0].rstrip(";") == "EXIT":
+        exit()
+
+    elif len(tokens) == 1 and tokens[0].endswith(";"):
+        stack_name = tokens[0][:-1]
+        stacks[stack_name] = Stack()
+
+    else:
+        command = tokens[0]
+        if command == "PUSH":
+            try:
+                value = eval(" ".join(tokens[1:-1]))
+            except NameError:
+                value = " ".join(tokens[1:-1])
+            stack_name = tokens[-1][:-1]
+            stacks[stack_name].push(value)
+        elif command == "POP":
+            stack_name = tokens[-1][:-1]
+            stacks[stack_name].pop()
+        elif command == "PRINT":
+            stack_name = tokens[-1][:-1]
+            stacks[stack_name].print()
+        elif command == "ADD":
+            stack_name = tokens[-1][:-1]
+            stacks[stack_name].add()
+        elif command == "SUBT":
+            stack_name = tokens[-1][:-1]
+            stacks[stack_name].subtract()
+        elif command == "MULT":
+            stack_name = tokens[-1][:-1]
+            stacks[stack_name].multiply()
+        elif command == "DIV":
+            stack_name = tokens[-1][:-1]
+            stacks[stack_name].divide()
+        elif command == "MOD":
+            stack_name = tokens[-1][:-1]
+            stacks[stack_name].mod()
 
 
 if __name__ == "__main__":
